18cdd04ff0ed7dfae6874b68b9fe1c1665252ed4
Geraldo B. I <43564683+geraldoarema@users.noreply.github.com>
2019-03-20 12:56:44 +0700

    Jobsheet 5 Finish

--- Geraldo-TI2B/Jobsheet 5/Geraldo-TI2B/src/app/app.component.ts
+++ Geraldo-TI2B/Jobsheet 5/Geraldo-TI2B/src/app/app.component.ts
@@ -29,5 +29,25 @@ export class AppComponent {
     console.log(index);
     this.coursesFor.splice(index,1);
   }
+
+  onChange(item){
+    item.name='Updated';
+  }
+  
+  coursesForOne;
+  loadCourses(){
+    this.coursesForOne=[
+    {id:1,nama:'coursesone1'},
+    {id:2,nama:'coursesone2'},
+    {id:3,nama:'coursesone3'},
+    {id:4,nama:'coursesone4'},
+    {id:5,nama:'coursesone5'},
+    ];
+  }
+
+  trackCourse(index,itemone){
+    return itemone ? itemone.id : undefined;
+  }
+
   
 }
