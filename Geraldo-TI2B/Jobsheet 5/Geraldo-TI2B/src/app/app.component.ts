4cfe45471b23dfc8a47a68d33103b6f549256370
Geraldo B. I <43564683+geraldoarema@users.noreply.github.com>
2019-03-20 10:26:29 +0700

    Jobsheet 5

--- Geraldo-TI2B/Jobsheet 5/Geraldo-TI2B/src/app/app.component.ts
+++ Geraldo-TI2B/Jobsheet 5/Geraldo-TI2B/src/app/app.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Belajar-angular';
-  courses=[];
+  courses=[1,2];
+
+  coursesFor=[
+    {id:1,name:'courses1'},
+    {id:2,name:'courses2'},
+    {id:3,name:'courses3'},
+    {id:4,name:'courses4'},
+    {id:5,name:'courses5'},
+  ]
+
+  viewMode='map';
+
+  onAdd(){
+    this.coursesFor.push({id:6,name:'courses6'});
+  }
+
+  onRemove(item){
+    console.log(item);
+    let index=this.coursesFor.indexOf(item);
+    console.log(index);
+    this.coursesFor.splice(index,1);
+  }
+  
 }
