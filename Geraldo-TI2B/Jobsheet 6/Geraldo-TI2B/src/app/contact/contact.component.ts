c5aed6a82ccc1901e6404e8bfb29f9b325c787c1
Geraldo B. I <43564683+geraldoarema@users.noreply.github.com>
2019-04-09 21:12:21 +0700

    Jobsheet 6

--- Geraldo-TI2B/Jobsheet 6/Geraldo-TI2B/src/app/contact/contact.component.ts
+++ Geraldo-TI2B/Jobsheet 6/Geraldo-TI2B/src/app/contact/contact.component.ts
@@ -16,9 +16,19 @@ import { Component, OnInit, Input } from '@angular/core';
 // }
 
   export class ContactComponent{
-    log(x){
-      console.log(x);
+    contactMethods=[
+      {id:1,name:'email'},
+      {id:2,name:'phone'}
+    ]
+
+    log(z){
+      console.log(z);
     }
+
+  submit(form){
+    console.log(form);
+    form.valid;
+  }
   }
 
 
